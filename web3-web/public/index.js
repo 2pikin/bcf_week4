eec8fbd99c06e70545d0cefa76cac0a8c822b091
Vasily Tupikin <2pikin@gmail.com>
2018-05-16 12:18:09 +0300

    add message keeper app

---
+++ web3-web/public/index.js
@@ -0,0 +1,472 @@
+$(document).ready(function() {
+
+  // get current message and donator from blockchain
+  const updateMessage = async () => {
+    const abi = [
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_message",
+            "type": "string"
+          }
+        ],
+        "name": "setMessage",
+        "outputs": [
+          {
+            "name": "success",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_price",
+            "type": "uint256"
+          }
+        ],
+        "name": "setPrice",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "price",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "lastDonator",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "message",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_newOwner",
+            "type": "address"
+          }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "name": "_token",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "name": "oldOwner",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "name": "newOwner",
+            "type": "address"
+          }
+        ],
+        "name": "TransferOwnership",
+        "type": "event"
+      }
+    ];
+    const AppContract = new web3.eth.Contract(abi, '0x37c80d0f2a06fcf664297992a7a9d091d2c9ba4c');
+    const message = await AppContract.methods.message().call();
+    const donator = await AppContract.methods.lastDonator().call();
+    $('#msg').text(message);
+    $('#donator').text(donator);
+  }
+
+  // update current user balance
+  const updateBalance = async () => {
+    const accounts  = await web3.eth.getAccounts();
+    const abi = [
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_from",
+            "type": "address"
+          },
+          {
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "payment",
+        "outputs": [
+          {
+            "name": "success",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_app",
+            "type": "address"
+          }
+        ],
+        "name": "setAppContract",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "transfer",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "appContract",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_newOwner",
+            "type": "address"
+          }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "name": "oldOwner",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "name": "newOwner",
+            "type": "address"
+          }
+        ],
+        "name": "OwnershipTransfer",
+        "type": "event"
+      }
+    ]
+    const MyTokenContract = new web3.eth.Contract(abi, '0x3c124f3139455b0Be40BFec2a74d164ea6bcd2a5');
+    const balance = await MyTokenContract.methods.balanceOf(accounts[0]).call();
+    $('#token').text(balance);
+    if (parseInt(balance) <= 0) {
+      $('.setupButton').addClass('disabled');
+    } else {
+      $('.setupButton').removeClass('disabled');
+    }
+  }
+
+  $('.tooltipped').tooltip();
+
+  if(typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+    console.log(`OK: ${web3.version}`);
+  } else {
+    alert('You must install MetaMask!');
+  }
+
+  updateBalance();
+  updateMessage();
+
+  $('.buyButton').click(async function() {
+    $('#progressbar').removeClass('hide');
+    const accounts  = await web3.eth.getAccounts();
+    await web3.eth.sendTransaction({
+      from: accounts[0],
+      to: '0x5c29c3cc99d2cfdb7813c695e02693d9dd2ef68e',
+      value: web3.utils.toWei($('#inputEthers').val(), 'ether')
+    }, function(error) {
+      console.log(error);
+    })
+    updateBalance();
+    $('#inputEthers').val('');
+    $('#progressbar').addClass('hide');
+  })
+
+  $('.setupButton').click(async function() {
+    $('#progressbar').removeClass('hide');
+    const accounts  = await web3.eth.getAccounts();
+    const abi = [
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_message",
+            "type": "string"
+          }
+        ],
+        "name": "setMessage",
+        "outputs": [
+          {
+            "name": "success",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_price",
+            "type": "uint256"
+          }
+        ],
+        "name": "setPrice",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "price",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "lastDonator",
+        "outputs": [
+          {
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "message",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_newOwner",
+            "type": "address"
+          }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "name": "_token",
+            "type": "address"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "name": "oldOwner",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "name": "newOwner",
+            "type": "address"
+          }
+        ],
+        "name": "TransferOwnership",
+        "type": "event"
+      }
+    ];
+    const AppContract = new web3.eth.Contract(abi, '0x37c80d0f2a06fcf664297992a7a9d091d2c9ba4c');
+    const tx = await AppContract.methods.setMessage($('#inputString').val()).send({
+      from: accounts[0]
+    });
+    $('#tx').html(`Transaction: <a target="_blank" href="https://rinkeby.etherscan.io/tx/${tx.transactionHash}">https://rinkeby.etherscan.io/tx/${tx.transactionHash}</a>`);
+    updateBalance();
+    updateMessage();
+    $('#inputString').val('');
+    $('#progressbar').addClass('hide');
+  })
+})
